500f47c632d979831a77672c4ef799059ae810ee
artemitut <92166551+artemitut@users.noreply.github.com>
2022-02-15 14:03:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// let year =prompt("Який зараз рік?", 2022)
+// if (year==2022){
+//     console.log("Ти молодець!")
+// }
+// else{
+//     console.log("Ти тупой")
+// }
+
+// let year = prompt("Коли був 2015 рік?")
+// if (year<2015){
+//     alert('Надзвичайно рано')
+// } 
+// else if (year>2015){
+//     alert('Надзвичайно пізно')
+// } 
+// if(year != String(year)) {
+//     alert = "11"
+// }
+// else{
+//     alert("Ты гений")
+// }
+
+// let x = 6
+// if ((x!=0)&(x>5)){
+//     alert("Ok")
+// }
+// else{
+//     alert("not ok")
+// }
+
+// function sayHi(){
+//     alert("Error 404")
+// }
+
+// sayHi()
+
+// function sayhi(n){
+//     return n*n
+// }
+
+// let x = prompt("VVedit chislo")
+// s = sayhi(x)
+// console.log(s)
